8aa1174b579f1a8960fc85460f428e951369e907
Maciej Garncarski <maciejgarncarski@protonmail.com>
2022-02-08 19:49:44 +0100

    fix: simplify code

--- src/scripts/shop/createOfferList.ts
+++ src/scripts/shop/createOfferList.ts
@@ -10,59 +10,31 @@ export const createOfferList = (data: offerTypes) => {
     const offersContainer = document.querySelector('.offers') as HTMLDivElement
     if (offersContainer) {
         offersContainer.textContent = ''
+        offersContainer.innerHTML = data
+            .map(({ name, price, discount, img, stars }) => {
+                const discountValue = discount * price
+                const withDiscount = `<p class="product__price product__price--discounted">${price}€</p><p class="product__discount">${discountValue}€</p>`
+                const wihoutDiscount = `<p class="product__price">${price}€</p>`
+
+                const starCount = [...Array(stars)]
+
+                const template = `
+                <div class="product">
+                    <img class="product__img" src="/images/shoes/${img}.png" alt="${name}" />
+                    <h2 class="product__name">${name}</h2>
+                    <div class="product__cart-btn">
+                        <span class="fas fa-cart-plus"></span>
+                    </div>
+                    <div class="product__price-container">
+                        ${discount !== 1 ? withDiscount : wihoutDiscount}
+                    </div>
+                    <div class="product__stars-container">
+                        ${starCount.map(() => `<span class="gold fas fa-star"></span>`).join('')}
+                    </div>
+                </div>
+            `
+                return template
+            })
+            .join('')
     }
-
-    const create = (element: string) => document.createElement(element)
-    const append = (parent: Element, child: Element) => parent.appendChild(child)
-    const addClass = (element: Element, className: string) => element.classList.add(className)
-    const setClass = (element: Element, className: string) => element.setAttribute('class', className)
-
-    data.forEach(({ name, price, discount, img, stars }) => {
-        const container = create('div')
-        setClass(container, 'product')
-        offersContainer?.appendChild(container)
-
-        const productImage = document.createElement('img')
-        productImage.src = `/images/shoes/${img}.png`
-        productImage.alt = name
-        setClass(productImage, 'product__img')
-        append(container, productImage)
-
-        const productName = create('h2')
-        productName.textContent = name
-        setClass(productName, 'product__name')
-        append(container, productName)
-
-        const priceContainer = create('div')
-        setClass(priceContainer, 'product__price-container')
-        append(container, priceContainer)
-
-        const priceTag = create('p')
-        priceTag.textContent = `${price.toString()}€`
-        setClass(priceTag, 'product__price')
-        append(priceContainer, priceTag)
-
-        if (discount !== 1) {
-            const discountTag = document.createElement('p')
-            const discountValue = discount * price
-            discountTag.textContent = `${Math.round(price - discountValue).toString()}€`
-            setClass(discountTag, 'product__discount')
-            addClass(priceTag, 'discounted')
-            append(priceContainer, discountTag)
-        }
-
-        const addToCart = create('div')
-        setClass(addToCart, 'product__cart-btn')
-        addToCart.innerHTML = `<span class="fas fa-cart-plus"></span>`
-        append(container, addToCart)
-
-        const starsContainer = create('div')
-        for (let i = 1; i <= stars; i++) {
-            const star = create('span')
-            setClass(star, 'gold fas fa-star')
-            append(starsContainer, star)
-        }
-        setClass(starsContainer, 'product__stars-container')
-        append(container, starsContainer)
-    })
 }
--- src/scripts/shop/filter.ts
+++ src/scripts/shop/filter.ts
@@ -2,46 +2,36 @@ import { offers } from '../../data/offers'
 import { createOfferList } from './createOfferList'
 
 const handleErrorMessage = () => {
-  const offersContainer = document.querySelector('.offers') as HTMLDivElement
-  if (offersContainer.innerHTML === '') {
-    offersContainer.innerHTML = "<p class='offers-error'>No items found.</p>"
-  }
+    const offersContainer = document.querySelector('.offers') as HTMLDivElement
+    if (offersContainer.innerHTML === '') {
+        offersContainer.innerHTML = "<p class='offers-error'>No items found.</p>"
+    }
 }
 
-const filter = (ev: SubmitEvent) => {
-  ev.preventDefault()
-  const minPrice = document.querySelector('#min-price') as HTMLInputElement
-  const maxPrice = document.querySelector('#max-price') as HTMLInputElement
-  const starsCount = document.querySelector('#star-count') as HTMLInputElement
+const filter = () => {
+    const minPrice = document.querySelector('#min-price') as HTMLInputElement
+    const maxPrice = document.querySelector('#max-price') as HTMLInputElement
+    const starsCount = document.querySelector('#star-count') as HTMLInputElement
 
-  const filteredOffers = offers.filter(({ price, discount, stars }) => {
-    let discountedPrice = price
-    if (discount !== 1) {
-      const discountValue = discount * price
-      discountedPrice = price - discountValue
-    }
-    const isPriceEmpty = (maxPrice.value === '' || minPrice.value === '')
-    const isStarCountEmpty = (starsCount.value === '' || starsCount.value === '0')
+    const filteredOffers = offers.filter(({ price, discount, stars }) => {
+        let discountedPrice = price
 
-    const filerByPrice = discountedPrice >= +minPrice.value && discountedPrice <= +maxPrice.value
-    const filterByStars = stars === +starsCount.value
-    const filterByEverything = stars === +starsCount.value && discountedPrice >= +minPrice.value && discountedPrice <= +maxPrice.value
+        if (discount !== 1) {
+            const discountValue = discount * price
+            discountedPrice = price - discountValue
+        }
 
-    if (isPriceEmpty && isStarCountEmpty) {
-      return offers
-    } else if (!isPriceEmpty && isStarCountEmpty) {
-      return filerByPrice
-    } else if (isPriceEmpty && !isStarCountEmpty) {
-      return filterByStars
-    } else {
-      return filterByEverything
-    }
-  })
-  createOfferList(filteredOffers)
-  handleErrorMessage()
+        const filterByMin = !+minPrice.value || +minPrice.value <= discountedPrice
+        const filterByMax = !+maxPrice.value || +maxPrice.value >= discountedPrice
+        const filterByStars = !starsCount.value || +starsCount.value === stars
+
+        return filterByMin && filterByMax && filterByStars
+    })
+    createOfferList(filteredOffers)
+    handleErrorMessage()
 }
 
 export const handleFilter = () => {
-  const form = document.querySelector('form')
-  form?.addEventListener('submit', filter)
+    const form = document.querySelector('form')
+    form?.addEventListener('input', filter)
 }
