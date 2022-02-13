f71c927ab418f8fda72234e2f4bfb7f61df9d6ac
Maciej Garncarski <maciejgarncarski@protonmail.com>
2022-02-13 21:58:45 +0100

    fix: footer error

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,11 @@ import './scss/style.scss'
 import { checkIsHomepage } from './scripts/checkIsHomepage'
 import { changePages } from './scripts/changePages'
 import { setActivePage } from './scripts/setActivePage'
-import { updateFooter } from './scripts/updateFooter'
 import { activePage } from './scripts/activePage'
 import { checkIsOnline } from './scripts/checkIsOnline'
 
 setActivePage()
 checkIsHomepage()
 changePages()
-updateFooter()
 activePage()
 checkIsOnline()
--- src/scripts/updateFooter.ts
+++ src/scripts/updateFooter.ts
@@ -2,7 +2,7 @@ const updateFooter = () => {
     const now = new Date()
     const year = now.getFullYear()
     const footerContent = `<a href="https://github.com/maciejgarncarski" target="_blank" rel="noopener norefferer">Maciej Garncarski</a>, ${year}`
-    // document.querySelector('.footer')!.innerHTML = footerContent
+    document.querySelector('.footer')!.innerHTML = footerContent
 }
 
 export { updateFooter }
