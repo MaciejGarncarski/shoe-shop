b0d64bafff2985fb09d44b771c77e13449ccea19
Maciej Garncarski <maciejgarncarski@protonmail.com>
2022-02-27 13:25:33 +0100

    add: conditional show/hide filters

--- src/scripts/shop/showFilters.ts
+++ src/scripts/shop/showFilters.ts
@@ -1,12 +1,17 @@
 const handleClick = () => {
   const btn = document.querySelector('.show-filters') as HTMLDivElement
-  const filters = document.querySelector('.filters')
-  filters?.classList.toggle('filters--active')
+  const filters = document.querySelector('.filters') as HTMLDivElement
   document.body.classList.toggle('locked')
-  btn?.classList.toggle('show-filters--active')
+  filters.classList.toggle('filters--active')
+  btn.classList.toggle('show-filters--active')
+  if (filters.classList.contains('filters--active')) {
+    btn.textContent = 'Hide Filters'
+  } else {
+    btn.textContent = 'Show Filters'
+  }
 }
 
 export const showFilters = () => {
-  const btn = document.querySelector('.show-filters')
-  btn?.addEventListener('click', handleClick)
+  const btn = document.querySelector('.show-filters') as HTMLDivElement
+  btn.addEventListener('click', handleClick)
 }
