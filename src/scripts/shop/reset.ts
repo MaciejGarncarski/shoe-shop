e8794fdc23e253268f38b03e40c3e39e98935de4
Maciej Garncarski <maciejgarncarski@protonmail.com>
2022-02-28 08:14:37 +0100

    fix: can't add items after reset

--- src/scripts/shop/reset.ts
+++ src/scripts/shop/reset.ts
@@ -1,9 +1,19 @@
 import { offers } from '../../data/offers'
 import { createOfferList } from './createOfferList'
+import { addToCart } from './addToCart'
+import { handleFilter } from './filter'
+import { showFilters } from './showFilters'
 
 export const resetForm = () => {
   const resetButton = document.querySelector('.filters__reset')
   if (resetButton) {
-    resetButton.addEventListener('click', () => createOfferList(offers))
+    resetButton.addEventListener('click', () => {
+      createOfferList(offers)
+      console.log('XD?')
+      addToCart()
+      handleFilter()
+      resetForm()
+      showFilters()
+    })
   }
 }
