6573fc0c86baf55a28cf6ce38089f2f3d7848ccc
Maciej Garncarski <maciejgarncarski@protonmail.com>
2022-06-20 11:18:17 +0200

    fix: bugfixes

--- src/scripts/shop/initFilter.ts
+++ src/scripts/shop/initFilter.ts
@@ -1,42 +1,40 @@
 import debounce from 'lodash.debounce'
 import { filterOffers } from './filterOffers'
 
+const getParentElement = (target: HTMLImageElement, isCheckbox?: boolean) => {
+  if (isCheckbox) {
+    return target.parentElement as HTMLDivElement
+  }
+  const parentOfImg = target.parentElement as HTMLLabelElement
+  return parentOfImg.parentElement as HTMLDivElement
+}
+
 export const initFilters = () => {
   const form = document.querySelector('form') as HTMLFormElement
   const starsContainer = document.querySelector('.filters__stars-container') as HTMLDivElement
   const stars = document.querySelectorAll<HTMLDivElement>('.filters__star')
-
   const starsArray = Array.from(stars)
   const activeClass = 'filters__star--active'
 
-  if (starsContainer) {
-    starsContainer.addEventListener('mouseleave', () => {
-      stars.forEach((star) => {
-        if (!star.classList.contains('clicked')) {
-          star.classList.remove(activeClass)
-        }
-      })
+  starsContainer.addEventListener('mouseleave', () => {
+    stars.forEach((star) => {
+      if (!star.classList.contains('clicked')) {
+        star.classList.remove(activeClass)
+      }
     })
-  }
+  })
 
   const removeActiveClass = () => stars.forEach((star) => star.classList.remove(activeClass, 'clicked'))
 
   const addStarClasses = (mouseEvent: MouseEvent, isCheckbox?: boolean) => {
-    removeActiveClass()
     const target = mouseEvent.target as HTMLImageElement
-    const getParentElement = () => {
-      if (isCheckbox) {
-        return target.parentElement as HTMLDivElement
-      }
-      const parentOfImg = target.parentElement as HTMLLabelElement
-      return parentOfImg.parentElement as HTMLDivElement
-    }
-    const targetParent = getParentElement()
+    const targetParent = getParentElement(target, isCheckbox)
     const hoveredStar = starsArray.indexOf(targetParent)
     const markedStars = starsArray.filter((markedStar) => starsArray.indexOf(markedStar) <= hoveredStar)
+
+    removeActiveClass()
+
     markedStars.forEach((markedStar) => {
-      const checkbox = markedStar.querySelector('.filters__star-checkbox') as HTMLInputElement
-      checkbox.checked = true
       if (isCheckbox) {
         markedStar.classList.add(activeClass, 'clicked')
       } else {
